c1bdebbf6d81dabdf66723026a75a5bc1d75d30f
Keith Smiley <keithbodesmiley@gmail.com>
2012-07-31 22:43:30 -0400

    Initial Commit

---
+++ Web Sharing/KSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KSAppDelegate.h
+//  Web Sharing
+//
+//  Created by Keith Smiley on 7/31/12.
+//  Copyright (c) 2012 Keith Smiley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Web Sharing/KSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  KSAppDelegate.m
+//  Web Sharing
+//
+//  Created by Keith Smiley on 7/31/12.
+//  Copyright (c) 2012 Keith Smiley. All rights reserved.
+//
+
+#import "KSAppDelegate.h"
+
+@implementation KSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
